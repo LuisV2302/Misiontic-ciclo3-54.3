dfe8e219d25a4d69e9c0673bcddc07521e4769d7
LuisV2302 <luesvilo2314@gmail.com>
2021-10-03 18:49:34 -0500

    Cambios con VUE

---
+++ src/pages/Inicio.vue
@@ -0,0 +1,121 @@
+<template>
+  <div style>
+    <div class="page-header page-header-small">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('img/bg6.jpg')"
+      >
+      </parallax>
+      <div class="content-center">
+        <div class="container d-flex justify-content-center"  id="contenedorPrincipal">
+          <h1 class="title">Bienvenido a Medictorio</h1>
+          <br>
+          <select id="seleccion" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+            <option selected="opt1">Filtra tus opciones</option>
+            <option value="opt2">Precio</option>
+            <option value="opt3">Especialidad</option>
+            <option value="opt4">calificación</option>
+            <option value="opt5">Ciudad</option>
+
+        </select>
+        <n-button type="primary" round block size="mini" id="botonbuscar" class="button center">Buscar</n-button>
+        </div>
+      </div>
+    </div>
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">¿Quiénes somos?</h2>
+            <h5 class="description">
+              Somos una página que está diseñada para las reservas de citas con médicos
+              particulares, nuestro objetivo es que sea un fácil acceso para los pacientes a sus
+              consultas médicas, siendo un servicio más personalizado, con un registro único para
+              los usuarios y su propio historial, para que lo consulten cuando lo vean necesario y
+              sea lo más amigable posible y fácil de usar
+            </h5>
+          </div>
+        </div>
+        <div class="separator separator-primary"></div>
+
+      </div>
+    </div>
+    <div class="section section-contact-us text-center">
+      <div class="container">
+        <h2 class="title">¿tienes preguntas?</h2>
+        <p class="description">Escríbenos</p>
+        <div class="row">
+          <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
+            <fg-input
+              class="input-lg"
+              placeholder="Escribe tu nombre"
+              v-model="form.firstName"
+              addon-left-icon="now-ui-icons users_circle-08"
+            >
+            </fg-input>
+            <fg-input
+              class="input-lg"
+              placeholder="Escribe tu correo"
+              v-model="form.email"
+              addon-left-icon="now-ui-icons ui-1_email-85"
+            >
+            </fg-input>
+            <div class="textarea-container">
+              <textarea
+                class="form-control"
+                name="name"
+                rows="4"
+                cols="80"
+                v-model="form.message"
+                placeholder="Déjanos tu mensaje"
+              ></textarea>
+            </div>
+            <div class="send-button">
+              <n-button type="primary" round block size="lg"
+                >enviar mensaje</n-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Button, FormGroupInput } from '@/components';
+export default {
+  name: 'inicio',
+  bodyClass: 'landing-page',
+  components: {
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput
+  },
+  data() {
+    return {
+      form: {
+        firstName: '',
+        email: '',
+        message: ''
+      }
+    };
+  }
+};
+</script>
+<style>
+  #seleccion{
+    width: 60%;
+    box-shadow: -4px 4px 7px -2px rgba(0,0,0,0.75);
+    margin-top: 13px;
+    margin-right: 15px;
+  }
+  #botonbuscar{
+    width: 20%;
+    align-self: center;
+  }
+  #contenedorPrincipal{
+    align-items: center;
+    align-content: center;
+  }
+
+
+</style>

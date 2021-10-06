38661b19cbc8db5eb9bfe11a9cbc8d67e7c9090e
Rinxs <acrinconalfonso@gmail.com>
2021-10-06 09:14:28 -0500

    Login realizado

---
+++ src/pages/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="page-header clear-filter" >
+    <div
+      class="page-header-image"
+         style="background-color:white"
+    ></div>
+    <div class="content">
+      <div class="container">
+        <div class="col-md-5 ml-auto mr-auto">
+          <card type="login" plain>
+            <div slot="header" class="logo-container">
+              <img v-lazy="'img/now-logo.jpeg'" alt="" />
+            </div>
+
+            <fg-input
+              class="no-border input-lg"
+              addon-left-icon="now-ui-icons users_circle-08"
+              placeholder="Usuario"
+            >
+            </fg-input>
+
+            <fg-input
+              class="no-border input-lg"
+              addon-left-icon="now-ui-icons text_caps-small"
+              placeholder="Contraseña"
+            >
+            </fg-input>
+
+            <template slot="raw-content" style="color:#276678">
+              <div class="card-footer text-center" >
+                <a
+                  href="#pablo"
+                  class="btn btn-primary btn-round btn-lg btn-block"
+                  >Ingresar</a
+                >
+              </div>
+              <div class="pull-left">
+                <h6>
+                  <a href="#pablo" class="link footer-link">Registrarse</a>
+                </h6>
+              </div>
+              <div class="pull-right">
+                <h6>
+                  <a href="#pablo" class="link footer-link">Ayuda</a>
+                </h6>
+              </div>
+            </template>
+          </card>
+        </div>
+      </div>
+    </div>
+    <main-footer></main-footer>
+  </div>
+</template>
+<script>
+import { Card, Button, FormGroupInput } from '@/components';
+import MainFooter from '@/layout/MainFooter';
+export default {
+  name: 'login-page',
+  bodyClass: 'login-page',
+  components: {
+    Card,
+    MainFooter,
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput
+  }
+};
+</script>
+<style>
+.page-header-image{
+  background: linear-gradient(rgba(39,102,120,1) 0%,rgba(211,224,234,1) 80%, rgba(246,245,245,1) 100%);
+}
+.btn-lg btn-block{
+  color:rgba(39,102,120,1);
+}
+</style>

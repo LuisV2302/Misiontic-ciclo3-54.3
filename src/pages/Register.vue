ec362fb8812d66f397fbbc9c14dc3034e942b0ca
CamiloRodriguezG <72152781+CamiloRodriguezG@users.noreply.github.com>
2021-10-10 16:45:35 -0500

    consumo de la API hecho

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -7,44 +7,98 @@
         <div class="col-md-5 ml-auto mr-auto">
           <card type="login" plain>
             <div slot="header" class="logo-container">
-              <img v-lazy="'img/logo-med.png'" alt="logo" />
+              <img class="img-logo" v-lazy="'img/logo-med.png'" alt="logo" />
             </div>
 
-            <fg-input class="no-border input-lg" placeholder="Username">
+            <fg-input
+              type="text"
+              class="no-border input-lg"
+              placeholder="Username"
+              v-model="usuario.username"
+              required
+            >
             </fg-input>
 
-            <fg-input class="no-border input-lg" placeholder="Nombre completo">
+            <fg-input
+              type="password"
+              class="no-border input-lg"
+              placeholder="Contraseña"
+              v-model="usuario.contraseña"
+              required
+            >
             </fg-input>
 
-            <fg-input class="no-border input-lg" placeholder="Correo">
+            <fg-input
+              type="text"
+              class="no-border input-lg"
+              placeholder="Nombre completo"
+              v-model="usuario.nombre"
+              required
+            >
             </fg-input>
 
-            <fg-input class="no-border input-lg" placeholder="Tipo de sangre">
+            <fg-input
+              type="email"
+              class="no-border input-lg"
+              placeholder="Correo"
+              v-model="usuario.correo"
+              required
+            >
             </fg-input>
 
-            <fg-input class="no-border input-lg" placeholder="Telefono">
+            <fg-input
+              class="no-border input-lg"
+              placeholder="Tipo de sangre"
+              v-model="usuario.tipoSangre"
+              required
+            >
+            </fg-input>
+
+            <fg-input
+              type="number"
+              class="no-border input-lg"
+              placeholder="Telefono"
+              v-model="usuario.telefono"
+              required
+            >
             </fg-input>
 
             <div class="roles-container">
-              <n-radio v-model="esDoctor" v-bind:value="true" label="1">Medico</n-radio>
-              <n-radio v-model="esDoctor" v-bind:value="false" label="2">Paciente</n-radio>
+              <n-radio v-model="esDoctor" v-bind:value="true" label="1"
+                >Medico</n-radio
+              >
+              <n-radio v-model="esDoctor" v-bind:value="false" label="0"
+                >Paciente</n-radio
+              >
             </div>
 
             <div v-if="esDoctor == true" class="medico-data">
-                <fg-input class="no-border input-lg" placeholder="Direccion consultorio">
-                </fg-input>
+              <fg-input
+                class="no-border input-lg"
+                placeholder="Direccion consultorio"
+                v-model="usuario.direccionConsultorio"
+                required
+              >
+              </fg-input>
 
-                <fg-input class="no-border input-lg" placeholder="Especialidad">
-                </fg-input>
+              <fg-input
+                class="no-border input-lg"
+                placeholder="Especialidad"
+                v-model="usuario.especialidad"
+                required
+              >
+              </fg-input>
             </div>
 
             <template slot="raw-content">
               <div class="card-footer text-center">
-                <a
+                <button
                   href="#pablo"
                   class="btn btn-primary btn-round btn-lg btn-block"
-                  >Crear cuenta</a
+                  v-on:click="crearUsuario"
                 >
+                  Crear cuenta
+                </button>
               </div>
               <div class="pull-left">
                 <h6>
@@ -65,9 +119,10 @@
   </div>
 </template>
 <script>
-import { Card, Button, FormGroupInput, Radio} from "@/components";
-import MainFooter from '@/layout/MainFooter';
-import MainNavbar from '@/layout/MainNavbar';
+import axios from "axios";
+import { Card, Button, FormGroupInput, Radio } from "@/components";
+import MainFooter from "@/layout/MainFooter";
+import MainNavbar from "@/layout/MainNavbar";
 export default {
   name: "login-page",
   bodyClass: "login-page",
@@ -82,22 +137,85 @@ export default {
   data() {
     return {
       esDoctor: null,
+      usuario: {
+        username: "",
+        contraseña: "",
+        nombre: "",
+        correo: "",
+        telefono: "",
+        tipoSangre: "",
+        direccionConsultorio: null,
+        especialidad: null,
+      },
     };
-  }
+  },
+  methods: {
+    crearUsuario() {
+      if (this.esDoctor == 1) {
+        let medico = {
+          username: this.usuario.username,
+          contraseña: this.usuario.contraseña,
+          nombre: this.usuario.nombre,
+          correo: this.usuario.correo,
+          telefono: this.usuario.telefono,
+          tipoSangre: this.usuario.tipoSangre,
+          direccionConsultorio: this.usuario.direccionConsultorio,
+          especialidad: this.usuario.especialidad,
+        };
+        axios
+          .post("http://localhost:8000/medicos/crear", this.usuario)
+          .then(() => {
+            console.log("medico agregado");
+            this.limpiarData();
+          });
+      } else {
+        let paciente = {
+          username: this.usuario.username,
+          contraseña: this.usuario.contraseña,
+          nombre: this.usuario.nombre,
+          correo: this.usuario.correo,
+          telefono: this.usuario.telefono,
+          tipoSangre: this.usuario.tipoSangre,
+        };
+        axios
+          .post("http://localhost:8000/pacientes/crear", paciente)
+          .then(() => {
+            console.log("paciente agregado");
+            this.limpiarData();
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    limpiarData() {
+      this.usuario = {
+        username: "",
+        contraseña: "",
+        nombre: "",
+        correo: "",
+        telefono: "",
+        tipoSangre: "",
+        direccionConsultorio: null,
+        especialidad: null,
+      };
+    },
+  },
 };
 </script>
 <style>
-.roles-container{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+.roles-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
-.medico-data{
-    margin-top: 15px;
+.medico-data {
+  margin-top: 15px;
 }
-.page-header-image{
-    /* background: linear-gradient(rgba(39,102,120,1) 0%,
+.page-header-image {
+  /* background: linear-gradient(rgba(39,102,120,1) 0%,
          rgba(211,224,234,1) 80%, rgba(246,245,245,1) 100%); */
-         background: white;
+  background: white;
+}
+.img-logo {
+  transform: scale(2);
 }
 </style>
